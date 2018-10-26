91939d14806bd92958d3d2311d2ba8c0986a8ef4
Igor <igor.sheremet@gmail.com>
2018-10-26 18:17:25 +0300

    2.1.04-Exercise-TextureDrawing

--- core/src/com/udacity/gamedev/MyGdxGame.kt
+++ core/src/com/udacity/gamedev/MyGdxGame.kt
@@ -2,35 +2,75 @@ package com.udacity.gamedev
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 
 class MyGdxGame : ApplicationAdapter() {
 
-    private lateinit var batch: SpriteBatch
+    companion object {
+        private val UDACITY_ORANGE = Color(
+                228.0f / 225.0f,
+                127.0f / 225.0f,
+                57.0f / 225.0f,
+                1.0f
+        )
+        const val WORLD_SIZE = 100.0f
+        const val LOGO_SIZE = 0.5f * WORLD_SIZE
+    }
+
+    private lateinit var viewport: ExtendViewport
+
+    // Declare a SpriteBatch
+    private lateinit var spriteBatch: SpriteBatch
+
+    // Declare a Texture for the Udacity Logo
     private lateinit var img: Texture
-    private val TAG = MyGdxGame::class.simpleName
 
     override fun create() {
+        viewport = ExtendViewport(WORLD_SIZE, WORLD_SIZE)
 
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        // Initialize the SpriteBatch
+        spriteBatch = SpriteBatch()
 
-        Gdx.app.log(TAG, "You're running on: ${Gdx.app.type}")
+        // Load the Udacity logo Texture (look in android/assets)
+        img = Texture("udacity_logo_white.png")
+
 
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        viewport.apply()
+        Gdx.gl.glClearColor(
+                UDACITY_ORANGE.r,
+                UDACITY_ORANGE.g,
+                UDACITY_ORANGE.b,
+                1f
+        )
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+
+        // Set the SpriteBatch's projection matrix
+        spriteBatch.projectionMatrix = viewport.camera.combined
+
+        // Begin the sprite spriteBatch
+        spriteBatch.begin()
+
+        // Centering the logo
+        spriteBatch.draw(
+                img,
+                viewport.worldWidth / 2 - LOGO_SIZE / 2,
+                viewport.worldHeight / 2 - LOGO_SIZE / 2,
+                LOGO_SIZE,
+                LOGO_SIZE
+        )
+
+        // End the sprite spriteBatch
+        spriteBatch.end()
     }
 
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height, true)
     }
 }
